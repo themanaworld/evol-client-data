69cb2293d84cf7f02e314b2ffbeda590f716fc48
Andrei Karas <akaras@inbox.ru>
2016-04-25 18:22:55 +0300

    Fix contributors ci script.

--- .tools/contributors.sh
+++ .tools/contributors.sh
@@ -5,7 +5,7 @@ source ./.tools/init.sh
 clientdata_init
 
 aptget_update
-aptget_install git-core gcc ca-certificates libxml2-utils make
+aptget_install git-core gcc ca-certificates libxml2-utils make xsltproc
 gitclone https://gitlab.com/evol/evol-tools.git tools
 
 cd tools/contrib_xsl
