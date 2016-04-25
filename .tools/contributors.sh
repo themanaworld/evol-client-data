b9d5f2466ba54df927607dea2a379cb1b76da725
Andrei Karas <akaras@inbox.ru>
2016-04-25 18:08:48 +0300

    Add contributors test job into .gitlab-ci.yml

---
+++ .tools/contributors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./.tools/init.sh
+
+clientdata_init
+
+aptget_update
+aptget_install git-core gcc ca-certificates libxml2-utils
+gitclone https://gitlab.com/evol/evol-tools.git tools
+
+cd tools/contrib_xsl
+
+make about-server
+cd ../../clientdata
+
+export RES=$(git diff)
+if [[ -n "${RES}" ]]; then
+    echo "Contributors list not updated"
+    echo "Diff:"
+    echo ${RES}
+    exit 1
+fi
