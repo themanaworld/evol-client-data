b382e8febbb0db53a0690194cec9574ba1e1b9eb
Andrei Karas <akaras@inbox.ru>
2016-06-02 00:39:31 +0300

    In ci scripts remove tools directory before clone.

--- .tools/deploy.sh
+++ .tools/deploy.sh
@@ -6,6 +6,7 @@ clientdata_init
 
 aptget_update
 aptget_install git-core zip gcc zlib1g-dev ca-certificates libxml2-utils python
+rm -rf tools
 gitclone https://gitlab.com/evol/evol-tools.git tools
 
 cd clientdata
