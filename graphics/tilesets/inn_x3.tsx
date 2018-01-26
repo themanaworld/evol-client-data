93e3eef6564d3301ca8e747ed5812d11eb2ddd8d
Saulc <lucashelaine14@gmail.com>
2018-01-26 02:13:26 +0100

    add new foods items

--- graphics/tilesets/inn_x3.tsx
+++ graphics/tilesets/inn_x3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="inn_x3" tilewidth="32" tileheight="96">
- <image source="../graphics/tiles/inn_x3.png" width="512" height="192"/>
+<tileset name="inn_x3" tilewidth="32" tileheight="96" tilecount="80" columns="16">
+ <image source="inn_x3.png" width="512" height="480"/>
 </tileset>
--- graphics/tilesets/wood_lab.tsx
+++ graphics/tilesets/wood_lab.tsx
@@ -1,24 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="wood_lab" tilewidth="96" tileheight="96">
- <image source="../graphics/tiles/wood_lab.png" width="384" height="288"/>
+<tileset name="wood_lab" tilewidth="96" tileheight="96" tilecount="12" columns="4">
+ <image source="wood_lab.png" width="384" height="288"/>
  <tile id="8">
-  <properties>
-   <property name="animation-delay0" value="5"/>
-   <property name="animation-delay1" value="5"/>
-   <property name="animation-delay2" value="5"/>
-   <property name="animation-delay3" value="5"/>
-   <property name="animation-delay4" value="5"/>
-   <property name="animation-delay5" value="5"/>
-   <property name="animation-delay6" value="5"/>
-   <property name="animation-delay7" value="5"/>
-   <property name="animation-frame0" value="1"/>
-   <property name="animation-frame1" value="2"/>
-   <property name="animation-frame2" value="3"/>
-   <property name="animation-frame3" value="4"/>
-   <property name="animation-frame4" value="5"/>
-   <property name="animation-frame5" value="6"/>
-   <property name="animation-frame6" value="7"/>
-   <property name="animation-frame7" value="0"/>
-  </properties>
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
  </tile>
 </tileset>
