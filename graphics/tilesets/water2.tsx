4a12000bf12fa5a1bb3bfdf16326a0231a603bc1
Saulc <lucashelaine14@gmail.com>
2018-02-10 14:05:47 +0100

    add water3 and make better visual for tulimsahr

--- graphics/tilesets/water2.tsx
+++ graphics/tilesets/water2.tsx
@@ -3,22 +3,37 @@
  <image source="water2.png" width="480" height="352"/>
  <tile id="120">
   <animation>
-   <frame tileid="120" duration="100"/>
-   <frame tileid="0" duration="100"/>
-   <frame tileid="1" duration="100"/>
-   <frame tileid="2" duration="100"/>
-   <frame tileid="3" duration="100"/>
-   <frame tileid="4" duration="100"/>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="120" duration="120"/>
+   <frame tileid="1" duration="1"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+   <frame tileid="4" duration="120"/>
+   <frame tileid="75" duration="1"/>
   </animation>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="animation-delay0" value="35"/>
+   <property name="animation-delay1" value="35"/>
+   <property name="animation-delay2" value="35"/>
+   <property name="animation-delay3" value="35"/>
+   <property name="animation-delay4" value="35"/>
+   <property name="animation-frame0" value="5"/>
+   <property name="animation-frame1" value="6"/>
+   <property name="animation-frame2" value="7"/>
+   <property name="animation-frame3" value="8"/>
+   <property name="animation-frame4" value="9"/>
+  </properties>
  </tile>
  <tile id="135">
   <animation>
-   <frame tileid="135" duration="100"/>
-   <frame tileid="15" duration="100"/>
-   <frame tileid="16" duration="100"/>
-   <frame tileid="17" duration="100"/>
-   <frame tileid="18" duration="100"/>
-   <frame tileid="19" duration="100"/>
+   <frame tileid="135" duration="200"/>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="16" duration="220"/>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="18" duration="220"/>
+   <frame tileid="19" duration="222"/>
   </animation>
  </tile>
  <tile id="150">
