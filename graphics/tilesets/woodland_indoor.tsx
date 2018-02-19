346fec054a3bc29aff2d1a0802f89a7e6dcb9e0f
Saulc <lucashelaine14@gmail.com>
2018-02-19 20:19:22 +0100

    add 005-7 map training house

--- graphics/tilesets/woodland_indoor.tsx
+++ graphics/tilesets/woodland_indoor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="woodland_indoor" tilewidth="32" tileheight="32">
- <image source="../graphics/tiles/woodland_indoor.png" width="512" height="512"/>
+<tileset name="woodland_indoor" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="woodland_indoor.png" width="512" height="512"/>
 </tileset>
