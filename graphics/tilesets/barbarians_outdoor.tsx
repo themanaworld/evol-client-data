a8ccbb8cca18d14b8b6cc6801d0bb950b465b2ae
Saulc <lucashelaine14@gmail.com>
2018-02-23 02:01:11 +0100

    add cave 007-1 mining camp

--- graphics/tilesets/barbarians_outdoor.tsx
+++ graphics/tilesets/barbarians_outdoor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="barbarians_outdoor" tilewidth="64" tileheight="64">
- <image source="../graphics/tiles/barbarians_outdoor.png" width="192" height="64"/>
+<tileset name="barbarians_outdoor" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="barbarians_outdoor.png" width="192" height="64"/>
 </tileset>
--- graphics/tilesets/cave2_sewer_mine_x2.tsx
+++ graphics/tilesets/cave2_sewer_mine_x2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave2_sewer_mine_x2" tilewidth="32" tileheight="64">
- <image source="../graphics/tiles/cave2_sewer_mine_x2.png" width="512" height="512"/>
+<tileset name="cave2_sewer_mine_x2" tilewidth="32" tileheight="64" tilecount="80" columns="16">
+ <image source="cave2_sewer_mine_x2.png" width="512" height="320"/>
 </tileset>
