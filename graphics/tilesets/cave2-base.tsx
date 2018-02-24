7ddbf71bbd703f099e749512dc15782ed6e38593
jesusalva <cpntb1@ymail.com>
2018-02-24 10:50:53 -0300

    Fix poorly named file to prevent testxml bugs (and for consistency)

--- graphics/tilesets/cave2-base.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave2-base" tilewidth="32" tileheight="32" tilecount="128" columns="16">
- <image source="cave2-base.png" width="512" height="256"/>
-</tileset>
---
+++ graphics/tilesets/cave2_base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cave2_base" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="cave2_base.png" width="512" height="256"/>
+</tileset>
