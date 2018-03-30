10f0a520b798e91dc659dcccac25dd0e787447cb
Saulc <lucashelaine14@gmail.com>
2018-03-30 15:26:52 +0200

    add big desert stone tsx

---
+++ graphics/tilesets/desertstone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="desertstone" tilewidth="32" tileheight="32" tilecount="96" columns="16">
+ <image source="desertstone.png" width="512" height="192"/>
+</tileset>
