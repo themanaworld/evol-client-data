f670ab047c318092d4d654659ddb8651bb6191d7
Saulc <lucashelaine14@gmail.com>
2018-04-03 21:00:49 +0200

    add proper  arena tsx

--- graphics/tilesets/arena.tsx
+++ graphics/tilesets/arena.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="arena" tilewidth="32" tileheight="32">
- <image source="../graphics/tiles/arena.png" width="512" height="512"/>
+<tileset name="arena" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="arena.png" width="512" height="512"/>
 </tileset>
