b5927d79e77b78c9bed1c9f8fb5aba44d070d82d
Jesusaves <cpntb1@ymail.com>
2018-05-08 18:56:22 -0300

    Fix stuff

--- graphics/tilesets/cave_bilevel.tsx
+++ graphics/tilesets/cave_bilevel.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave_bilevel" tilewidth="32" tileheight="32">
- <image source="../graphics/tiles/cave_bilevel.png" width="512" height="512"/>
+<tileset name="cave_bilevel" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="cave_bilevel.png" width="512" height="512"/>
 </tileset>
--- graphics/tilesets/woodland_x4.tsx
+++ graphics/tilesets/woodland_x4.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="woodland_x4" tilewidth="32" tileheight="128">
- <image source="../graphics/tiles/woodland_x4.png" width="512" height="128"/>
+<tileset name="woodland_x4" tilewidth="32" tileheight="128" tilecount="16" columns="16">
+ <image source="woodland_x4.png" width="512" height="128"/>
 </tileset>
