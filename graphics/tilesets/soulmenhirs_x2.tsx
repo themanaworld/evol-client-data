2795429f502adc83aabfebd14f48f6d043055aed
Jesusaves <cpntb1@ymail.com>
2018-09-10 11:14:20 -0300

    Remove unused duplicated file (now recognized as an NPC, let's not overload atlas)

--- graphics/tilesets/soulmenhirs_x2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="soulmenhirs_x2" tilewidth="64" tileheight="64" tilecount="8" columns="8">
- <image source="../sprites/npcs/sprite/soulmenhirs_x2.png" width="512" height="64"/>
-</tileset>
