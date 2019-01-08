2c919b5cb18f1cb8714b2a2a1e4cccc2dea04415
Jesusaves <cpntb1@ymail.com>
2019-01-08 01:43:01 -0200

    Fix bug in several maps due bad tsx file %%e Do not mess with them Saulc %%e

--- graphics/tilesets/indoor-accessories.tsx
+++ graphics/tilesets/indoor-accessories.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="inn" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="indoor-accessories.png" width="512" height="512"/>
+<tileset name="accessories" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="indoor-accessories.png" width="640" height="640"/>
 </tileset>
