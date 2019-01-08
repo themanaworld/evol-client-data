cce576b78f976926dbee1e37feabd9ed61143ebc
Jesusaves <cpntb1@ymail.com>
2019-01-08 01:43:57 -0200

    [skip ci] Let's change this too, to prevent further bugs

--- graphics/tilesets/signs.tsx
+++ graphics/tilesets/signs.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="signs_x2" tilewidth="32" tileheight="64" tilecount="24" columns="8">
+<tileset name="signs x2" tilewidth="32" tileheight="64" tilecount="24" columns="8">
  <image source="signs_x2.png" width="256" height="192"/>
 </tileset>
