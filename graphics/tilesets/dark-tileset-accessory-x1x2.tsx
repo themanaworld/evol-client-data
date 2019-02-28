f5d785884701fa37befac3cf217877585b3fe30d
Micksha <ms-shaman@gmx.de>
2019-02-28 14:12:27 +0000

    Darktileset

---
+++ graphics/tilesets/dark-tileset-accessory-x1x2.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dark-tileset-accessory-x1x2" tilewidth="32" tileheight="64" tilecount="64" columns="8">
+ <image source="dark-tileset-accessory-x1x2.png" width="256" height="512"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="150"/>
+   <frame tileid="20" duration="150"/>
+   <frame tileid="21" duration="150"/>
+   <frame tileid="22" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ graphics/tilesets/fire-cauldron.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fire-cauldron" tilewidth="32" tileheight="64" tilecount="10" columns="10">
+ <image source="fire-cauldron.png" width="320" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
